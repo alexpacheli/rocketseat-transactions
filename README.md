08f016a3258d2b2a37ca36019b6cc764efa3c57b
Alex Pacheli <alex@pacheli.com.br>
2023-09-14 23:08:35 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# RF requisitos funcionais
+[x] o usuário pode criar uma nova transação;
+[x] o usuário deve poder obter um resumo da conta;
+[x] o usuário deve poder listar todas as transações que já ocorreram;
+[x] o usuário deve poder visualizar uma transação unica;
+
+# RN regras de negocio
+[x] a transação pode ser do tipo crédito que somará ao valor total, ou débito que subtrairá;
+[ ] deve ser possível identificarmos o usuário entre as requisições;
+[ ] o usuário só pode visualizar transações à qual ele criou;
+
+# RNF regras não funcionais (parte tech)
+[ ]
---
+++ test/transactions.spec.ts
@@ -0,0 +1,85 @@
+import { it, beforeAll, afterAll, describe, expect, beforeEach } from 'vitest'
+import { execSync } from 'node:child_process'
+import request from 'supertest'
+import { app } from '../src/app'
+
+describe('Transactions routes', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  beforeEach(() => {
+    execSync('npm run knex migrate:rollback --all')
+    execSync('npm run knex migrate:latest')
+  })
+
+  it('should be able to create a new transaction', async () => {
+    await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'New Transaction',
+        amount: 5000,
+        type: 'credit',
+      })
+      .expect(201)
+  })
+
+  it.skip('should be able to listen all transactions', async () => {
+    const createTransactionResponse = await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'New Transaction',
+        amount: 5000,
+        type: 'credit',
+      })
+
+    const cookies = createTransactionResponse.get('Set-Cookie')
+
+    const listTransactionsResponse = await request(app.server)
+      .get('/transactions')
+      .set('Cookie', cookies)
+      .expect(200)
+
+    expect(listTransactionsResponse.body.transactions).toEqual([
+      expect.objectContaining({
+        title: 'New transaction',
+        amount: 5000,
+      }),
+    ])
+  })
+
+  it.skip('should be able to get a specific transaction', async () => {
+    const createTransactionResponse = await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'New Transaction',
+        amount: 5000,
+        type: 'credit',
+      })
+
+    const cookies = createTransactionResponse.get('Set-Cookie')
+
+    const listTransactionsResponse = await request(app.server)
+      .get('/transactions')
+      .set('Cookie', cookies)
+      .expect(200)
+
+    const transactionId = listTransactionsResponse.body.transactions[0].id
+
+    const getTransactionsResponse = await request(app.server)
+      .get(`/transactions/${transactionId}`)
+      .set('Cookie', cookies)
+      .expect(200)
+
+    expect(getTransactionsResponse.body.transaction).toEqual(
+      expect.objectContaining({
+        title: 'New transaction',
+        amount: 5000,
+      }),
+    )
+  })
+})
