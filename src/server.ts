805c19e1639db39f49dd7ae33b270019ac98665f
Alex Pacheli <alex@pacheli.com.br>
2023-09-14 23:59:39 -0300

    Added host

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,7 @@ import { env } from './env'
 app
   .listen({
     port: env.PORT,
-    host: 'RENDER' in process.env ? '0.0.0.' : 'localhost',
+    host: 'RENDER' in process.env ? '0.0.0.0' : 'localhost',
   })
   .then(() => {
     console.log('HTTP Server Running')
